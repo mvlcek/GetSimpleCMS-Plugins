d470dd9a625b2f6b5752996233db40cc8a917ba6
mvlcek <mvlcek@gmx.de>
2023-01-27 19:21:29 +0100

    i18n, i18n_specialpages: language cs

---
+++ plugins/i18n_base/lang/cs_CZ.php
@@ -0,0 +1,18 @@
+<?php
+// by tomish
+$i18n = array(
+    'CLOSE_ALL' => "Zobrazit pouze nejvyšší stránky",
+    'DEFAULT_LANGUAGE_DESCR' => "Výchozí jazyk stránek s URL bez jazykového suffixu (např. index), např. )\"de\"",
+    'FILTER' => "Filtr",
+    'FILTER_TITLE' => "Hledat podle části titulku a/nebo tagu/klíčkového slova",
+    'NEW_LANGUAGE_DESCR' => "Jestliže chcete přidat nový jazyk, stačí vytvořit novou stránku mající v url suffix se zkratkou jazyka (např. pro češtinu \"index_cs\")",
+    'OPEN_ALL' => "Zobrazit všechny stránky",
+    'PAGES' => "Zobrazit všechny stránky (I18N)",
+    'SAVE_DEFAULT_LANGUAGE' => "Uložit",
+    'SAVE_FAILURE' => "<b>Chyba:</b> Nevalidní kód jazyka. Použijte např. \"en\" (english), \"de\" (german), \"es\" (spanish), \"fr\" (french), ...",
+    'SAVE_OPEN' => "Uložit aktuální stav výpisu stránek",
+    'SAVE_SUCCESS' => "Jazyk byl korektně uložen.",
+    'VIEW_HIERARCHICAL' => "Hierarchicky",
+    'VIEW_TITLE' => "Podle názvu",
+    'PLUGIN_DESCRIPTION' => "Umožňuje vytvářet mnohojazyčný obsah na základě parametru v slug/URL názvu"
+);
---
+++ plugins/i18n_navigation/lang/cs_CZ.php
@@ -0,0 +1,20 @@
+<?php
+// by tomish
+$i18n = array(
+    'NAVIGATION' => "Struktura menu front endu",
+    'EDIT_NAVIGATION' => "Editovat strukturu menu",
+    'TOP' => "na začátku",
+    'INSERT_AFTER' => "vložit po",
+    'NAVIGATION_DESCR' => "Seřaďte stránky pomocí táhnutím položek myší. Zašedlé položky nebudou v menu vidět. Text menu lze editovat po kliknutí na něj.",
+    'LANGUAGE_DESCR' => "Aktuálně zobrazujete obsah v jazyce",
+    'SAVE_NAVIGATION' => "Uložit strukturu menu",
+    'INDENT' => "Odsadit",
+    'OUTDENT' => "Zrušit odsazení",
+    'SAVE_FAILURE' => "Vaše změny struktury menu nemohou být uloženy. Ujistěte se, zda existuje složka backups/i18n_navigation s právy zápisu.",
+    'SAVE_SUCCESS' => "Vaše změny struktury menu byly uloženy.",
+    'UNDO_FAILURE' => "Struktura menu nemůže být plně obnovena.",
+    'UNDO_SUCCESS' => "Struktura menu byla plně obnovena.",
+    'TOGGLE_MENU' => "Přepnout viditelnost v menu",
+    'TOGGLE_PRIVATE' => "Přepnout privátní mód",
+    'PLUGIN_DESCRIPTION' => "Multilevel navigation & breadcrumbs (I18N enabled)"
+);
